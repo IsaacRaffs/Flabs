2e16e6a8703b5285b9c02b7b5e71c50cabe408ad
IsaacRaffs <isaacrafaelmores.dev@gmail.com>
2025-12-02 11:46:58 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,7 @@ function Home(){
     return <h1>Gleydson (<a>NIGGA</a>) after stole doces of hallowenn</h1> 
 }
 
+function Video(){
+    return <video src="../sources/hallo.mp4"></video>
+}
 export default Home;
