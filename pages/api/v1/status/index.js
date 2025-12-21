9632f725dac4d0343a25ed38d0d16568292215a8
IsaacRaffs <isaacrafaelmoraes.dev@gmail.com>
2025-12-21 18:14:24 -0300

    adding more tests and building api/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
+import { version } from "react";
 
-async function Status(request, response) {
-  const res = await database.query("select 1 + 1;");
-  console.log(res.rows);
-  response.status(200).json({ chave: "servidor online" });
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+
+  const databaseMaxConnectionsValue =
+    databaseMaxConnections.rows[0].max_connections;
+
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE datname ='local_user';",
+  );
+
+  console.log(databaseOpenedConnectionsResult.rows);
+
+  response.status(200).json({
+    updated_At: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+      },
+    },
+  });
 }
 
-export default Status;
+export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("GET to /api/v1/status should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.version).toEqual("18.1");
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  expect(responseBody.dependencies.database.openned_connections).toEqual(1);
 });
