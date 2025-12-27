c769048d0fdbf50b56902f7eb763b3dcb7055527
IsaacRaffs <isaacrafaelmoraes.dev@gmail.com>
2025-12-27 17:56:15 -0300

    fixing environment variables

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
+    transaction: process.env.POOL_MODE,
   });
 
   console.log("Credenciais do postgres", {
