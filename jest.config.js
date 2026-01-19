f2acc5689b1f91044e4cb14a6d7b91523d3050e0
IsaacRaffs <isaacrafaelmoraes.dev@gmail.com>
2026-01-19 20:03:55 -0300

    fixing jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
-import nexJs from "next/jest.js";
+import nextJest from "next/jest.js";
 import dotenv from 'dotenv';
 
 dotenv.config({
   path: '.env.development',
 });
  
-const createJestConfig = nexJs();
+const createJestConfig = nextJest({
+  dir: "./",
+});
 
 const jestConfig = createJestConfig({
-  setupFiles: ["dotenv/config"],
   moduleDirectories: ["node_modules", "<rootDir>"]
 });
 
