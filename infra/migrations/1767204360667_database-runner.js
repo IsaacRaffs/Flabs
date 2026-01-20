2e0fa6140546665944f817ae325b7c75fef09db2
IsaacRaffs <isaacrafaelmoraes.dev@gmail.com>
2026-01-19 22:05:31 -0300

    fixing migrations 01

--- infra/migrations/1767204360667_database-runner.js
+++ infra/migrations/1767204360667_database-runner.js
@@ -1,18 +1,5 @@
-/**A
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
 export const shorthands = undefined;
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const up = (pgm) => {};
+export const up = function up(pgm) {};
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const down = (pgm) => {};
+export const down = function down(pgm) {};
