992eb8064338629c36176e1d889bde07668728be
IsaacRaffs <isaacrafaelmoraes.dev@gmail.com>
2026-02-05 06:28:28 -0300

    adds eslint to code and reforcing CI on github

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,12 @@ import js from "@eslint/js";
 
 export default defineConfig([
   globalIgnores(["**/node_modules/", ".git/", ".next/", "pages/"]),
-	{
-		files: ["**/*.js"],
-		plugins: { js, },
-		rules: {
+  {
+    files: ["**/*.js"],
+    plugins: { js },
+    rules: {
       "no-duplicate-imports": "error",
-			"no-unused-vars": "warn",
-		},
-	},
+      "no-unused-vars": "warn",
+    },
+  },
 ]);
-
